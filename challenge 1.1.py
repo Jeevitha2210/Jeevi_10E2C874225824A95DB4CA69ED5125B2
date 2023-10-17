7570904b8d6c6782cc81a5cbedd03b0543716ed9
Jeevitha2210 <148204840+Jeevitha2210@users.noreply.github.com>
2023-10-17 15:26:40 +0530

    Add files via upload

---
+++ challenge 1.1.py
@@ -0,0 +1,9 @@
+#Implementing a recursive function to find the factorial of a number 
+def fact_rec (n):
+  if n ==0 or n==1:
+    return 1 
+  else :
+    return n *fact_rec (n-1 )
+number =2
+result =fact_rec (number )
+print ("the factorial of{} is{}".format (number,result ))
